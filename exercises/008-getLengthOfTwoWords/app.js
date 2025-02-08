2a78d81e5ba029019965c37b18dced88620c67e1
MilanoTester <eliasmilano.tester@gmail.com>
2025-02-08 16:35:06 -0300

    exercises 6 to 10

--- exercises/008-getLengthOfTwoWords/app.js
+++ exercises/008-getLengthOfTwoWords/app.js
@@ -1,4 +1,6 @@
 function getLengthOfTwoWords(word1, word2) {
   // your code here
-  
+ let totalLength = word1.length + word2.length;
+ return totalLength;
 }
+console.log(getLengthOfTwoWords("some", "words"));
--- exercises/009-getLengthOfThreeWords/app.js
+++ exercises/009-getLengthOfThreeWords/app.js
@@ -1,6 +1,7 @@
 function getLengthOfThreeWords(word1, word2, word3) {
   // your code here
-  
+  let totalLength = word1.length + word2.length + word3.length;
+  return totalLength;
 }
 
 let output = getLengthOfThreeWords('some', 'other', 'words');
