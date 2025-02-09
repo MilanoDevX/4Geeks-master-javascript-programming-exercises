8e4f2d5c1b6d4280b4d932ca93878c41a876f2b0
MilanoTester <eliasmilano.tester@gmail.com>
2025-02-08 22:05:41 -0300

    exercises 46 to 50

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,18 @@
 // Write your function here
+function computeAverageOfNumbers(arr) {
+    if (arr.length == 0) {
+        return 0;
+    } else {
+        let sum = 0
+        arr.forEach(item => {
+            sum = sum + item;
+            return sum;
+        })
+        let average = sum / arr.length;
+        return average;
+    }
+}
+
+let input = [1, 2, 3, 4, 5];
+let output = computeAverageOfNumbers(input);
+console.log(output); // --> 3
