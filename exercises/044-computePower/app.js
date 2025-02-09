81aeb9951c531bf985ed6404eaa2dad43f2cc4f7
MilanoTester <eliasmilano.tester@gmail.com>
2025-02-08 21:45:16 -0300

    exercises 40 to 45

--- exercises/044-computePower/app.js
+++ exercises/044-computePower/app.js
@@ -1,6 +1,11 @@
 function computePower(num, exponent) {
   // your code here
+  let power = 1;
+  for (let i=1; i<= exponent; i++) {
+    power = power * num;
+  }
+  return power;
 }
 
-let output = computePower(2, 3);
+let output = computePower(3, 2);
 console.log(output); // --> 8
